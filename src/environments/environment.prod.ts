3f4d29f2884e022c619801ad1a39a430b55aac0f
gomeznaiker <gomeznaiker@gmail.com>
2025-05-16 15:02:34 +0000

    style: Se agrego diseño nuevo y mas moderno utilizando material 3 de google

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  supabaseUrl : 'https://wsuekobcyerazpqmvtcj.supabase.co',
+  supabaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzdWVrb2JjeWVyYXpwcW12dGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMjUwMDUsImV4cCI6MjA2MjkwMTAwNX0.FAzaENKZ2964NCk0mILCIbjqjfvCyMtShAOIYQA5Deo',
   firebaseConfig: {
     apiKey: "AIzaSyC-uxdFY7tkrXB3PVbJPTzURc-Fp2lXxi8",
     authDomain: "callconnect-49e3a.firebaseapp.com",
